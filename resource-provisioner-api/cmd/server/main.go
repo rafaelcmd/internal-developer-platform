97549cca861f7c0f7543d18ac0f151d79579a642
Rafael Costa <rafaelcmd@gmail.com>
2025-03-09 20:22:47 -0300

    removing kafka conf

--- resource-provisioner-api/cmd/server/main.go
+++ resource-provisioner-api/cmd/server/main.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	"github.com/rafaelcmd/cloud-ops-manager/api/internal/core/usecase"
-	httpdelivery "github.com/rafaelcmd/cloud-ops-manager/api/internal/delivery/http"
-	"github.com/rafaelcmd/cloud-ops-manager/api/repository"
 	"log"
 	"net/http"
 )
 
 func main() {
-	brokers := []string{"kafka:9092"}
-	topic := "resource-provisioner"
+	//brokers := []string{"kafka:9092"}
+	//topic := "resource-provisioner"
 
-	repo := repository.NewKafkaResourceRepository(brokers, topic)
-	defer repo.Close()
+	//repo := repository.NewKafkaResourceRepository(brokers, topic)
+	//defer repo.Close()
 
-	useCase := usecase.NewResourceUseCase(repo)
+	//useCase := usecase.NewResourceUseCase(repo)
 
-	router := httpdelivery.SetupRoutes(useCase)
+	//router := httpdelivery.SetupRoutes(useCase)
 	log.Printf("Server running on port 5000")
-	http.ListenAndServe(":5000", router)
+	http.ListenAndServe(":5000", nil) //router
 }
