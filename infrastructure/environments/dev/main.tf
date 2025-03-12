b3d6564e116d3f4d6b5e6c57612665907b626b31
Rafael Costa <rafaelcmd@gmail.com>
2025-03-12 19:27:57 +0000

    modules with github path

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -1,16 +1,16 @@
-module "aws_networking" {
+module "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/compute/ec2?ref=main" {
   source = "../../modules/aws/networking"
   environment = var.environment
 }
 
-module "aws_security" {
+module "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/networking?ref=main" {
   source = "../../modules/aws/security"
   environment = var.environment
   vpc_id = module.aws_networking.vpc_id
   private_key = module.aws_ec2.private_key
 }
 
-module "aws_ec2" {
+module "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/security?ref=main" {
   source = "../../modules/aws/compute/ec2"
   environment = var.environment
   public_subnet_id = module.aws_networking.public_subnet_id
