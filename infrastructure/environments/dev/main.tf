95004e5d3c801202b8162b067f1ce995bd06d613
Rafael Costa <rafaelcmd@gmail.com>
2025-03-12 20:25:09 +0000

    adjusted modules

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -1,4 +1,4 @@
-module "aws_ec2" {
+module "aws_networking" {
   source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/networking?ref=main"
   environment = var.environment
 }
@@ -10,7 +10,7 @@ module "aws_security" {
   private_key = module.aws_ec2.private_key
 }
 
-module "aws_networking" {
+module "aws_ec2" {
   source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/compute/ec2?ref=main"
   environment = var.environment
   public_subnet_id = module.aws_networking.public_subnet_id
