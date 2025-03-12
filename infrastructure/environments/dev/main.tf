ddf5c913f9e64f9774be7fff5e0e0a21d6863b2d
Rafael Costa <rafaelcmd@gmail.com>
2025-03-12 19:42:32 +0000

    modules with github path adjusted

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -1,17 +1,17 @@
-module "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/compute/ec2?ref=main" {
-  source = "../../modules/aws/networking"
+module "ec2" {
+  source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/networking?ref=main"
   environment = var.environment
 }
 
-module "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/networking?ref=main" {
-  source = "../../modules/aws/security"
+module "security" {
+  source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/security?ref=main"
   environment = var.environment
   vpc_id = module.aws_networking.vpc_id
   private_key = module.aws_ec2.private_key
 }
 
-module "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/security?ref=main" {
-  source = "../../modules/aws/compute/ec2"
+module "networking" {
+  source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/compute/ec2?ref=main"
   environment = var.environment
   public_subnet_id = module.aws_networking.public_subnet_id
   security_group_id = module.aws_security.security_group_id
