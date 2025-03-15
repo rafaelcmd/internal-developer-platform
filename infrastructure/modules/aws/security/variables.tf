6aae99b5637a999a7c611b9b624b4a4e7b6856c4
Rafael Costa <rafaelcmd@gmail.com>
2025-03-15 12:15:11 -0300

    removing unused variable

--- infrastructure/modules/aws/security/variables.tf
+++ infrastructure/modules/aws/security/variables.tf
@@ -2,8 +2,3 @@ variable "vpc_id" {
   description = "VPC ID for the security group"
   type        = string
 }
-
-variable "private_key" {
-  description = "Private key name for the EC2 instance"
-  type        = string
-}
