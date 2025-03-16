7e860bdbf1d90fa4f9529bc4f3f1b99176419040
Rafael Costa <rafaelcmd@gmail.com>
2025-03-16 09:17:04 -0300

    changed workspace name

--- infrastructure/environments/dev/tfe_variables.tf
+++ infrastructure/environments/dev/tfe_variables.tf
@@ -2,13 +2,13 @@ provider "tfe" {
   hostname = "app.terraform.io"
 }
 
-resource "tfe_workspace" "cloudops_workspace" {
+resource "tfe_workspace" "cloud-ops-manager-workspace" {
   name         = "cloud-ops-manager-workspace"
   organization = "cloudops-manager-org"
 }
 
 resource "tfe_variable" "ec2_host" {
-  workspace_id = tfe_workspace.cloudops_workspace.id
+  workspace_id = tfe_workspace.cloud-ops-manager-workspace.id
   key          = "RESOURCE_PROVISIONER_API_EC2_HOST"
   value        = module.aws_ec2.resource_provisioner_api_host
   category     = "env"
@@ -16,14 +16,14 @@ resource "tfe_variable" "ec2_host" {
 }
 
 resource "tfe_variable" "ec2_username" {
-  workspace_id = tfe_workspace.cloudops_workspace.id
+  workspace_id = tfe_workspace.cloud-ops-manager-workspace.id
   key          = "RESOURCE_PROVISIONER_API_EC2_USERNAME"
   value        = module.aws_ec2.resource_provisioner_api_username
   category     = "env"
 }
 
 resource "tfe_variable" "ec2_private_key" {
-  workspace_id = tfe_workspace.cloudops_workspace.id
+  workspace_id = tfe_workspace.cloud-ops-manager-workspace.id
   key          = "RESOURCE_PROVISIONER_API_EC2_PRIVATE_KEY"
   value        = module.aws_ec2.resource_provisioner_api_private_key
   category     = "env"
