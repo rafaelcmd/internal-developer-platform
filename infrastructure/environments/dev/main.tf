d4acaf8a41fe95c8c5b68f0152f7ed74f1dd7f0d
Rafael Costa <rafaelcmd@gmail.com>
2025-03-16 12:59:46 -0300

    added ec2 outputs on root

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -12,3 +12,16 @@ module "aws_ec2" {
   public_subnet_id = module.aws_networking.public_subnet_id
   security_group_id = module.aws_security.security_group_id
 }
+
+output "resource_provisioner_api_host" {
+  value = module.aws_ec2.resource_provisioner_api_host
+}
+
+output "resource_provisioner_api_username" {
+  value = module.aws_ec2.resource_provisioner_api_username
+}
+
+output "resource_provisioner_api_private_key" {
+  value     = module.aws_ec2.resource_provisioner_api_private_key
+  sensitive = true
+}
