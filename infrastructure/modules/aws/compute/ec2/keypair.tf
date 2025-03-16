b38feeb6ab51441beb5ec796b38388e566b1f834
Rafael Costa <rafaelcmd@gmail.com>
2025-03-16 19:39:49 -0300

    secrets manager moved to ec2 module

--- infrastructure/modules/aws/compute/ec2/keypair.tf
+++ infrastructure/modules/aws/compute/ec2/keypair.tf
@@ -1,6 +1,16 @@
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
-    rsa_bits = 4096
+  rsa_bits = 4096
+}
+
+resource "aws_secretsmanager_secret" "resource-provisioner-api-private-key" {
+  name = "resource-provisioner-api-private-key"
+  description = "Private key for resource-provisioner-api"
+}
+
+resource "aws_secretsmanager_secret_version" "resource-provisioner-api-private-key" {
+  secret_id = aws_secretsmanager_secret.resource-provisioner-api-private-key.id
+  secret_string = tls_private_key.ssh_key.private_key_pem
 }
 
 resource "aws_key_pair" "generated_key" {
