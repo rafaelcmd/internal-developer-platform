2c3623f7397bb09586cde5d7f39272c75b31cac5
Rafael Costa <rafaelcmd@gmail.com>
2025-03-18 20:07:45 -0300

    default values for parameters

--- infrastructure/modules/aws/compute/ec2/parameters.tf
+++ infrastructure/modules/aws/compute/ec2/parameters.tf
@@ -1,11 +1,11 @@
 resource "aws_ssm_parameter" "resource_provisioner_api_host" {
   name  = "/RESOURCE_PROVISIONER_API/EC2_PUBLIC_IP"
   type  = "String"
-  value = ""
+  value = "RESOURCE_PROVISIONER_API_EC2_PUBLIC_IP"
 }
 
 resource "aws_ssm_parameter" "resource_provisioner_api_username" {
   name  = "/RESOURCE_PROVISIONER_API/EC2_USERNAME"
   type  = "String"
-  value = ""
+  value = "RESOURCE_PROVISIONER_API_EC2_USERNAME"
 }
