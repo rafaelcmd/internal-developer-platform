58a0c5f98e9b053f4164a6660a68795cea7e7154
Rafael Costa <rafaelcmd@gmail.com>
2025-03-22 15:38:45 -0300

    changed to use ec2_instance_connect

--- infrastructure/modules/aws/compute/ec2/main.tf
+++ infrastructure/modules/aws/compute/ec2/main.tf
@@ -1,10 +1,51 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "resource-provisioner-api-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "resource-provisioner-api-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role_policy" "ec2_instance_connect" {
+  name = "AllowEC2InstanceConnect"
+  role = aws_iam_role.ec2_role.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2-instance-connect:SendSSHPublicKey"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_instance" "resource-provisioner-api" {
-  ami = "ami-08b5b3a93ed654d19"
-  instance_type = "t2.micro"
-  subnet_id = var.public_subnet_id
-  vpc_security_group_ids = [var.security_group_id]
-  key_name = aws_key_pair.generated_key.key_name
+  ami                         = "ami-08b5b3a93ed654d19"
+  instance_type               = "t2.micro"
+  subnet_id                   = var.public_subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
   associate_public_ip_address = true
+
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
 
   tags = {
     Name = "resource-provisioner-api"
--- infrastructure/modules/aws/compute/ec2/outputs.tf
+++ infrastructure/modules/aws/compute/ec2/outputs.tf
@@ -5,3 +5,7 @@ output "resource_provisioner_api_host" {
 output "resource_provisioner_api_username" {
   value = "ec2-user"
 }
+
+output "resource_provisioner_api_instance_id" {
+  value = aws_instance.resource-provisioner-api.id
+}
