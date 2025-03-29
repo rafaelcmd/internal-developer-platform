6bcfc79f44668e0ec634ab9935035b1253b12f49
Rafael Costa <rafaelcmd@gmail.com>
2025-03-29 16:34:35 -0300

    fix: output  name

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -29,14 +29,14 @@ module "aws_sqs_queue" {
 }
 
 // Criar pasta para outputs
-output "resource_provisioner_api_host" {
+output "cloud_ops_manager_api_host" {
   value = module.aws_ec2.resource_provisioner_api_host
 }
 
-output "resource_provisioner_api_username" {
+output "cloud_ops_manager_api_username" {
   value = module.aws_ec2.resource_provisioner_api_username
 }
 
-output "resource_provisioner_api_instance_id" {
+output "cloud_ops_manager_api_instance_id" {
   value = module.aws_ec2.resource_provisioner_api_instance_id
 }
