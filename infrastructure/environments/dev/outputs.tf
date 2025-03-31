29ea17941ba885830c7bced03c5e54977350387b
Rafael Costa <rafaelcmd@gmail.com>
2025-03-31 19:08:33 -0300

    refactor: created sqs queue url output

---
+++ infrastructure/environments/dev/outputs.tf
@@ -0,0 +1,15 @@
+output "cloud_ops_manager_api_host" {
+  value = module.aws_ec2.resource_provisioner_api_host
+}
+
+output "cloud_ops_manager_api_username" {
+  value = module.aws_ec2.resource_provisioner_api_username
+}
+
+output "cloud_ops_manager_api_instance_id" {
+  value = module.aws_ec2.resource_provisioner_api_instance_id
+}
+
+output "cloud_ops_manager_sqs_queue_url" {
+  value = module.aws_sqs_queue.sqs_queue_url
+}
