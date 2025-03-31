5d632637a6768e4b6510580e88068aa06c99d127
Rafael Costa <rafaelcmd@gmail.com>
2025-03-31 19:45:41 -0300

    refactor: created sqs queue url output

--- infrastructure/modules/aws/sqs/outputs.tf
+++ infrastructure/modules/aws/sqs/outputs.tf
@@ -2,3 +2,8 @@ output "sqs_queue_arn" {
   description = "SQS Queue ARN for the Resource Provisioner"
   value = aws_sqs_queue.resource_provisioner_queue.arn
 }
+
+output "sqs_queue_url" {
+  description = "SQS Queue URL for the Resource Provisioner"
+  value = aws_sqs_queue.resource_provisioner_queue.url
+}
