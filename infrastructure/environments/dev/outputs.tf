d5b61ba7fea433d7017d255353b7b996ed5b6e89
Rafael Costa <rafaelcmd@gmail.com>
2025-04-01 19:20:45 -0300

    fix: adjust parameter value

--- infrastructure/environments/dev/outputs.tf
+++ infrastructure/environments/dev/outputs.tf
@@ -9,7 +9,3 @@ output "cloud_ops_manager_api_username" {
 output "cloud_ops_manager_api_instance_id" {
   value = module.aws_ec2.resource_provisioner_api_instance_id
 }
-
-output "cloud_ops_manager_sqs_queue_url" {
-  value = module.aws_sqs_queue.sqs_queue_url
-}
