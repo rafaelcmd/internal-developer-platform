ccf1f695653d11795142b4fd9a7621ef6e610fd8
Rafael Costa <rafaelcmd@gmail.com>
2025-04-06 16:01:19 -0300

    fix: terraform fmt

--- infrastructure/modules/aws/sqs/outputs.tf
+++ infrastructure/modules/aws/sqs/outputs.tf
@@ -1,11 +1,11 @@
 output "sqs_queue_arn" {
   description = "SQS Queue ARN for the Resource Provisioner"
-  value = aws_sqs_queue.resource_provisioner_queue.arn
+  value       = aws_sqs_queue.resource_provisioner_queue.arn
 }
 
 output "sqs_queue_url" {
   description = "SQS Queue URL for the Resource Provisioner"
-  value = aws_sqs_queue.resource_provisioner_queue.url
+  value       = aws_sqs_queue.resource_provisioner_queue.url
 }
 
 output "sqs_queue_parameter_arn" {
