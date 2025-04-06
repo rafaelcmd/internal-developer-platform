d900b460377707bc30e3bcc4c35760d53c061cc0
Rafael Costa <rafaelcmd@gmail.com>
2025-04-06 14:45:34 -0300

    chore: ec2_host variable

--- infrastructure/modules/aws/security/main.tf
+++ infrastructure/modules/aws/security/main.tf
@@ -1,26 +1,33 @@
-resource "aws_security_group" "resource-provisioner-api-sg" {
+resource "aws_security_group" "cloud_ops_manager_api_sg" {
+  name        = "cloud-ops-manager-api-sg"
+  description = "Security group for CloudOps Manager API"
   vpc_id = var.vpc_id
 
   ingress {
+    description = "Allow SSH access"
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ec2_host]
+  }
+
+  ingress {
+    description = "Allow HTTP access from API Gateway"
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_security_group_rule" "allow_http" {
-  security_group_id = aws_security_group.resource-provisioner-api-sg.id
-  type              = "ingress"
-  from_port         = 5000
-  to_port           = 5000
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  tags = {
+    Name = "cloud-ops-manager-api-sg"
+  }
 }
--- infrastructure/modules/aws/security/variables.tf
+++ infrastructure/modules/aws/security/variables.tf
@@ -2,3 +2,8 @@ variable "vpc_id" {
   description = "VPC ID for the security group"
   type        = string
 }
+
+variable "ec2_host" {
+  description = "IP address for SSH access"
+  type        = string
+}
