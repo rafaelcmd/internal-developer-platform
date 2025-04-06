c68731e9449a0cbab469d48c54da15ed9b58b3d1
Rafael Costa <rafaelcmd@gmail.com>
2025-04-06 14:43:33 -0300

    chore: ec2_host variable

--- infrastructure/environments/dev/main.tf
+++ infrastructure/environments/dev/main.tf
@@ -6,6 +6,7 @@ module "aws_security" {
   source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infrastructure/modules/aws/security?ref=main"
 
   vpc_id = module.aws_networking.vpc_id
+  my_ip  = module.aws_ec2.cloud_ops_manager_api_ec2_host
 }
 
 module "aws_ec2" {
