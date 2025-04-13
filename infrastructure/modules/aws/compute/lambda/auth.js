e2827c7de2978ef7b8c37748f0f31a246abb588b
Rafael Costa <rafaelcmd@gmail.com>
2025-04-13 16:04:51 -0300

    fix: adjusted lambda code

--- infrastructure/modules/aws/compute/lambda/auth.js
+++ infrastructure/modules/aws/compute/lambda/auth.js
@@ -1,27 +1,37 @@
-const AWS = require('aws-sdk');
-const cognito = new AWS.CognitoIdentityServiceProvider();
+import { CognitoIdentityProviderClient, InitiateAuthCommand } from '@aws-sdk/client-cognito-identity-provider';
+import { SSMClient, GetParameterCommand } from '@aws-sdk/client-ssm';
 
-exports.handler = async (event) => {
+const cognitoClient = new CognitoIdentityProviderClient();
+const ssmClient = new SSMClient();
+
+export const handler = async (event) => {
     try {
         const body = JSON.parse(event.body);
 
-        const params = {
+        const parameterResponse = await ssmClient.send(new GetParameterCommand({
+            Name: '/cognito/userPoolId',
+            WithDecryption: true
+        }));
+
+        const clientId = parameterResponse.Parameter?.Value;
+
+        const command = new InitiateAuthCommand({
             AuthFlow: 'USER_PASSWORD_AUTH',
-            ClientId: await new AWS.SSM().getParameter({ Name: '/CLOUD_OPS_MANAGER_COGNITO/CLIENT_ID' }).promise().then(data => data.Parameter.Value),
+            ClientId: clientId,
             AuthParameters: {
                 USERNAME: body.username,
                 PASSWORD: body.password
             }
-        };
+        });
 
-        const response = await cognito.initiateAuth(params).promise();
+        const authResponse = await cognitoClient.send(command);
 
         return {
             statusCode: 200,
             body: JSON.stringify({
-                accessToken: response.AuthenticationResult.AccessToken,
-                idToken: response.AuthenticationResult.IdToken,
-                refreshToken: response.AuthenticationResult.RefreshToken
+                accessToken: authResponse.AuthenticationResult.AccessToken,
+                idToken: authResponse.AuthenticationResult.IdToken,
+                refreshToken: authResponse.AuthenticationResult.RefreshToken
             }),
             headers: {
                 'Content-Type': 'application/json',
