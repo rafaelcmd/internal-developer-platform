43a277ea4a5b482eade5156726b3f7e348760da6
Rafael Costa <rafaelcmd@gmail.com>
2025-04-13 19:59:00 -0300

    fix: created ssm get parameter policy to lambda role

--- infrastructure/modules/aws/compute/lambda/main.tf
+++ infrastructure/modules/aws/compute/lambda/main.tf
@@ -42,7 +42,7 @@ resource "aws_lambda_permission" "api_gateway_invoke_auth" {
   function_name = aws_lambda_function.auth_lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn    = "${var.api_execution_arn}*/*/*"
+  source_arn = "${var.api_execution_arn}*/*/*"
 }
 
 resource "aws_iam_policy" "lambda_cognito_policy" {
@@ -50,20 +50,43 @@ resource "aws_iam_policy" "lambda_cognito_policy" {
   description = "Policy for Lambda to access Cognito"
 
   policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
+    Version = "2012-10-17"
+    Statement = [
       {
-          Effect = "Allow"
-          Action = [
+        Effect = "Allow"
+        Action = [
           "cognito-idp:InitiateAuth",
-          ]
-          Resource = "*"
+        ]
+        Resource = "*"
       }
-      ]
+    ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_cognito_policy_attachment" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = aws_iam_policy.lambda_cognito_policy.arn
+}
+
+resource "aws_iam_policy" "ssm_get_parameter" {
+  name        = "ssm_get_parameter_policy"
+  description = "Policy for Lambda to access SSM parameters"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParameter",
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_get_parameter_attachment" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.ssm_get_parameter.arn
 }
