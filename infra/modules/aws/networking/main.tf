f2eaa3d41848e5acef17e7d82f4756f17e11e104
Rafael Costa <rafaelcmd@gmail.com>
2025-04-20 14:08:25 -0300

    fix: changed the cidr blocks of private subnets

--- infra/modules/aws/networking/main.tf
+++ infra/modules/aws/networking/main.tf
@@ -32,7 +32,7 @@ resource "aws_subnet" "cloud_ops_manager_public_subnet_b" {
 
 resource "aws_subnet" "cloud_ops_manager_private_subnet_a" {
   vpc_id            = aws_vpc.cloud_ops_manager_vpc.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.3.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
@@ -42,7 +42,7 @@ resource "aws_subnet" "cloud_ops_manager_private_subnet_a" {
 
 resource "aws_subnet" "cloud_ops_manager_private_subnet_b" {
   vpc_id            = aws_vpc.cloud_ops_manager_vpc.id
-  cidr_block        = "10.0.2.0/24"
+  cidr_block        = "10.0.4.0/24"
   availability_zone = "us-east-1b"
 
   tags = {
