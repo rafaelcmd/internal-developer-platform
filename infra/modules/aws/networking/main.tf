8e43d0775e5f2f59af1283e13da8e1998cb67ab8
Rafael Costa <rafaelcmd@gmail.com>
2025-04-20 15:32:26 -0300

    fix: terraform fmt

--- infra/modules/aws/networking/main.tf
+++ infra/modules/aws/networking/main.tf
@@ -67,13 +67,13 @@ resource "aws_eip" "cloud_ops_manager_nat_b" {
 }
 
 resource "aws_nat_gateway" "cloud_ops_manager_nat_gateway_a" {
-  allocation_id     = aws_eip.cloud_ops_manager_nat_a.id
-  subnet_id         = aws_subnet.cloud_ops_manager_public_subnet_a.id
+  allocation_id = aws_eip.cloud_ops_manager_nat_a.id
+  subnet_id     = aws_subnet.cloud_ops_manager_public_subnet_a.id
 }
 
 resource "aws_nat_gateway" "cloud_ops_manager_nat_gateway_b" {
-  allocation_id     = aws_eip.cloud_ops_manager_nat_b.id
-  subnet_id         = aws_subnet.cloud_ops_manager_public_subnet_b.id
+  allocation_id = aws_eip.cloud_ops_manager_nat_b.id
+  subnet_id     = aws_subnet.cloud_ops_manager_public_subnet_b.id
 }
 
 resource "aws_route_table" "cloud_ops_manager_public_route_table" {
--- infra/modules/aws/s3/main.tf
+++ infra/modules/aws/s3/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "cloud_ops_manager_consumer_deploy_bucket" {
-  bucket = "cloud-ops-manager-consumer-deploy-bucket"
+  bucket        = "cloud-ops-manager-consumer-deploy-bucket"
   force_destroy = true
 
   tags = {
