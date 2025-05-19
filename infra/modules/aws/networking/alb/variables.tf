7e0a46fc8f12950b202ce01702cf9181d3d545cc
Rafael Costa <rafaelcmd@gmail.com>
2025-05-19 20:26:53 +0000

    created alb module

---
+++ infra/modules/aws/networking/alb/variables.tf
@@ -0,0 +1,14 @@
+variable "cloud_ops_manager_api_security_group_id" {
+  type        = string
+  description = "Security Group ID"
+}
+
+variable "cloud_ops_manager_api_public_subnet_ids" {
+  type = list(string)
+  description = "Public subnet ids list"
+}
+
+variable "cloud_ops_manager_vpc_id" {
+  description = "VPC ID for the ALB"
+  type        = string
+}
