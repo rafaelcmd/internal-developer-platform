5dc893b5bfb20107d8359221199e68e5567380ed
Rafael Costa <rafaelcmd@gmail.com>
2025-05-25 10:49:54 -0300

    refactor: removed cloudwatch and ADOT ssm documents and parameters

--- infra/modules/aws/compute/ec2/parameters.tf
+++ infra/modules/aws/compute/ec2/parameters.tf
@@ -22,45 +22,6 @@ resource "aws_ssm_parameter" "cloud_ops_manager_consumer_ec2_instance_id" {
   value = aws_instance.cloud_ops_manager_consumer_ec2.id
 }
 
-resource "aws_ssm_parameter" "cloud_ops_manager_api_cloudwatch_agent_config" {
-  name = "/CloudOpsManager/CloudWatchAgentConfig-API"
-  type = "String"
-  value = jsonencode({
-    agent = {
-      metrics_collection_interval = 60
-      run_as_user                 = "root"
-    },
-    logs = {
-      logs_collected = {
-        files = {
-          collect_list = [
-            {
-              file_path       = "/var/log/cloud-ops-manager-api.log"
-              log_group_name  = "/aws/ec2/cloud-ops-manager-api"
-              log_stream_name = "cloud-ops-manager-api-{instance_id}"
-            }
-          ]
-        }
-      }
-    },
-    metrics = {
-      append_dimensions = {
-        InstanceId = "{InstanceId}"
-      },
-      metrics_collected = {
-        cpu = {
-          measurement = [
-            "cpu_usage_idle",
-            "cpu_usage_user",
-            "cpu_usage_system"
-          ],
-          metrics_collection_interval = 60
-        }
-      }
-    }
-  })
-}
-
 resource "aws_ssm_parameter" "cloud_ops_manager_consumer_cloudwatch_agent_config" {
   name = "/CloudOpsManager/CloudWatchAgentConfig-Consumer"
   type = "String"
@@ -98,28 +59,6 @@ resource "aws_ssm_parameter" "cloud_ops_manager_consumer_cloudwatch_agent_config
       }
     }
   })
-}
-
-resource "aws_ssm_parameter" "cloud_ops_manager_api_adot_collector_xray_config" {
-  name = "/CloudOpsManager/ADOTCollectorConfig-API"
-  type = "String"
-
-  value = <<-EOT
-    receivers:
-      otlp:
-        protocols:
-          grpc:
-          http:
-
-    exporters:
-      awsxray:
-
-    service:
-      pipelines:
-        traces:
-          receivers: [otlp]
-          exporters: [awsxray]
-  EOT
 }
 
 resource "aws_ssm_parameter" "cloud_ops_manager_consumer_adot_collector_xray_config" {
