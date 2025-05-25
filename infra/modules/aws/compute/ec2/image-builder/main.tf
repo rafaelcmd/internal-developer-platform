5a72e8b43a7dbf475a490c3b0923d93910394de7
Rafael Costa <rafaelcmd@gmail.com>
2025-05-25 10:45:53 -0300

    feat: created image builder component to install and configure ADOT

--- infra/modules/aws/compute/ec2/image-builder/main.tf
+++ infra/modules/aws/compute/ec2/image-builder/main.tf
@@ -142,6 +142,97 @@ resource "aws_iam_instance_profile" "image_builder_profile" {
 }
 
 # ------------------------------------------------------------------------------
+# Component for ADOT Installation and Configuration
+# ------------------------------------------------------------------------------
+resource "aws_imagebuilder_component" "adot_install_and_configure" {
+  name        = "adot-install-and-configure"
+  platform = "Linux"
+  version     = "1.0.0"
+  description = "Install and configure ADOT"
+
+  data = <<EOT
+  name: Install and Configure ADOT
+  description: Install and configure the AWS Distro for OpenTelemetry Collector
+  schemaVersion: 1.0
+  phases:
+    - name: build
+      steps:
+        - name: Install ADOT
+          action: ExecuteBash
+          inputs:
+            commands:
+              - set -e
+              - yum install -y unzip curl
+              - cd /tmp
+              - curl -LO https://aws-otel-collector.s3.amazonaws.com/amazon_linux/amd64/latest/aws-otel-collector.rpm
+              - rpm -Uvh aws-otel-collector.rpm
+
+        - name: Configure ADOT
+          action: ExecuteBash
+          inputs:
+            commands:
+              - mkdir -p /opt/aws/aws-otel-collector
+              - cat <<EOF > /opt/aws/aws-otel-collector/aws-otel-collector-config.yaml
+                receivers:
+                  otlp:
+                    protocols:
+                      grpc:
+                      http:
+
+                processors:
+                  batch:
+
+                exporters:
+                  awsxray:
+                  awssemf:
+                  awcloudwatchlogs:
+                    log_group_name: /aws/cloodops-manager-logs
+                    log_stream_name: instance-{instance_id}
+                    region: "us-east-1"
+
+                service:
+                  pipelines:
+                    traces:
+                      receivers: [otlp]
+                      processors: [batch]
+                      exporters: [awsxray]
+
+                    metrics:
+                      receivers: [otlp]
+                      processors: [batch]
+                      exporters: [awssemf, awcloudwatchlogs]
+
+                    logs:
+                      receivers: [otlp]
+                      processors: [batch]
+                      exporters: [awscloudwatchlogs]
+  EOF
+
+    - name: CreateSystemdService
+        action: ExecuteBash
+        inputs:
+          commands:
+            - if [ ! -f /etc/systemd/system/aws-otel-collector.service ]; then
+            -   cat <<SERVICE > /etc/systemd/system/aws-otel-collector.service
+            - [Unit]
+            - Description=AWS OTel Collector
+            - After=network.target
+            -
+            - [Service]
+            - ExecStart=/opt/aws/aws-otel-collector/bin/aws-otel-collector --config /opt/aws/aws-otel-collector/aws-otel-collector-config.yaml
+            - Restart=always
+            -
+            - [Install]
+            - WantedBy=multi-user.target
+            - SERVICE
+            - fi
+            - systemctl daemon-reload
+            - systemctl enable aws-otel-collector
+            - systemctl start aws-otel-collector
+  EOT
+}
+
+# ------------------------------------------------------------------------------
 # Image Recipe
 # ------------------------------------------------------------------------------
 resource "aws_imagebuilder_image_recipe" "image_builder_recipe" {
@@ -158,8 +249,14 @@ resource "aws_imagebuilder_image_recipe" "image_builder_recipe" {
     }
   }
 
+  # Install OS updates
   component {
     component_arn = "arn:aws:imagebuilder:us-east-1:aws:component/update-linux/1.0.2/1"
+  }
+
+  # Install and configuring ADOT
+  component {
+    component_arn = aws_imagebuilder_component.adot_install_and_configure.arn
   }
 
   description = "CloudOps Manager Base Image"
