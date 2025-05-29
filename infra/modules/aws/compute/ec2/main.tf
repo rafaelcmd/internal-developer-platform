8e71e7a64a646b7f174ba294671602b4e36ba624
Rafael Costa <rafaelcmd@gmail.com>
2025-05-29 08:13:53 -0300

    refactor: updated the EC2 AMI

--- infra/modules/aws/compute/ec2/main.tf
+++ infra/modules/aws/compute/ec2/main.tf
@@ -2,7 +2,7 @@
 # API EC2 Instance
 # ------------------------------------------------------------------------------
 resource "aws_instance" "cloud_ops_manager_api_ec2" {
-  ami                         = "ami-098e0177d0a9b8cd0"
+  ami                         = "ami-01686b4d6520be3e7"
   instance_type               = "t2.micro"
   subnet_id                   = var.cloud_ops_manager_public_subnet_id
   vpc_security_group_ids      = [var.cloud_ops_manager_api_security_group_id]
@@ -103,7 +103,7 @@ resource "aws_iam_role_policy" "cloud_ops_manager_api_observability" {
 # Consumer EC2 Instance
 # ------------------------------------------------------------------------------
 resource "aws_instance" "cloud_ops_manager_consumer_ec2" {
-  ami                    = "ami-098e0177d0a9b8cd0"
+  ami                    = "ami-01686b4d6520be3e7"
   instance_type          = "t2.micro"
   subnet_id              = var.cloud_ops_manager_private_subnet_id
   vpc_security_group_ids = [var.cloud_ops_manager_consumer_security_group_id]
