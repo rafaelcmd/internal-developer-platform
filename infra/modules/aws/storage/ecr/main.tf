9702c2b184c494e987390ccec0be7b7fd9b22b48
Rafael Costa <rafaelcmd@gmail.com>
2025-05-31 13:31:47 -0300

    feat: created specific remote backend for ecr

--- infra/modules/aws/storage/ecr/main.tf
+++ infra/modules/aws/storage/ecr/main.tf
@@ -1,3 +1,25 @@
+terraform {
+  required_version = ">= 1.11.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
+terraform {
+  cloud {
+
+    organization = "cloudops-manager-org"
+
+    workspaces {
+      name = "cloudops-manager-ecr-repository"
+    }
+  }
+}
+
 resource "aws_ecr_repository" "cloud_ops_manager_ecr_repository" {
   name                 = "cloud_ops_manager_ecr_repository"
   image_tag_mutability = "MUTABLE"
