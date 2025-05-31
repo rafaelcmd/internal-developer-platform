2b6dc451e256dd23898123e65628d20739f3627d
Rafael Costa <rafaelcmd@gmail.com>
2025-05-31 11:30:43 -0300

    feat: changed the api dockerfile and ecs

--- infra/modules/aws/compute/containers/ecs/main.tf
+++ infra/modules/aws/compute/containers/ecs/main.tf
@@ -2,40 +2,47 @@ resource "aws_ecs_cluster" "cloud_ops_manager_api_cluster" {
   name = "cloud-ops-manager-api-cluster"
 }
 
-resource "aws_iam_role" "cloud_ops_manager_api_task_role" {
-  name               = "cloud-ops-manager-api-task-role"
-  assume_role_policy = data.aws_iam_policy_document.cloud_ops_manager_api_assume_role_policy.json
+resource "aws_iam_role" "cloud_ops_manager_api_ecs_task_execution_role" {
+  name               = "cloud-ops-manager-api-ecs-task-execution-role"
+
+  assume_role_policy = jsondecode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
 }
 
-data "aws_iam_policy_document" "cloud_ops_manager_api_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "cloud_ops_manager_api_task_role_policy" {
-  role       = aws_iam_role.cloud_ops_manager_api_task_role.name
+resource "aws_iam_role_policy_attachment" "cloud_ops_manager_api_ecs_task_execution_role_policy" {
+  role       = aws_iam_role.cloud_ops_manager_api_ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_ecs_task_definition" "cloud_ops_manager_api_task" {
+resource "aws_cloudwatch_log_group" "cloud_ops_manager_api_ecs_log_group" {
+    name              = "/ecs/cloud-ops-manager"
+    retention_in_days = 7
+}
+
+resource "aws_ecs_task_definition" "cloud_ops_manager_api_task_definition" {
   family                   = "cloud-ops-manager-api-task"
-  task_role_arn            = aws_iam_role.cloud_ops_manager_api_task_role.arn
-  execution_role_arn       = aws_iam_role.cloud_ops_manager_api_task_role.arn
-  network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "512"
+
+  execution_role_arn = aws_iam_role.cloud_ops_manager_api_ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
       name      = "cloud-ops-manager-api"
-      image     = "ami-077c7efa4f912139d"
-      essential = true
+      image     = "${var.api_repository_url}:latest"
+
       portMappings = [
         {
           containerPort = 5000
@@ -43,6 +50,15 @@ resource "aws_ecs_task_definition" "cloud_ops_manager_api_task" {
           protocol      = "tcp"
         }
       ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options   = {
+          "awslogs-group"         = aws_cloudwatch_log_group.cloud_ops_manager_api_ecs_log_group.name
+          "awslogs-region"        = "us-east-1"
+          "awslogs-stream-prefix" = "ecs-api"
+        }
+      }
     }
   ])
 }
---
+++ infra/modules/aws/compute/containers/ecs/variables.tf
@@ -0,0 +1,4 @@
+variable "api_repository_url" {
+  description = "The URL of the ECR repository for the Cloud Ops Manager API."
+  type        = string
+}
