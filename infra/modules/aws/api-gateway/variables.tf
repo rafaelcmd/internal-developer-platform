c955a6f41aba9a6ccc0678ac66dc1aec0a7d25c6
Rafael Costa <rafaelcmd@gmail.com>
2025-06-24 10:25:36 -0300

    refactor: removed EC2 to use only containers

--- infra/modules/aws/api-gateway/variables.tf
+++ infra/modules/aws/api-gateway/variables.tf
@@ -1,8 +1,3 @@
-variable "cloud_ops_manager_api_host" {
-  description = "Host of the Cloud Ops Manager API"
-  type        = string
-}
-
 variable "auth_lambda_invoke_arn" {
   description = "Invoke ARN of the Lambda function for authentication"
   type        = string
--- infra/modules/aws/compute/ec2/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "cloud_ops_manager_api_ec2_host" {
-  value = aws_instance.cloud_ops_manager_api_ec2.public_ip
-}
