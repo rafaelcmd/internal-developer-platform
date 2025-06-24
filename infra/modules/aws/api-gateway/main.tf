825777ccea4da74465cf8c4e09e810fb84677403
Rafael Costa <rafaelcmd@gmail.com>
2025-06-24 10:34:10 -0300

    refactor: removed APIGateway EC2 integrations and deployments

--- infra/modules/aws/api-gateway/main.tf
+++ infra/modules/aws/api-gateway/main.tf
@@ -28,31 +28,6 @@ resource "aws_api_gateway_method_response" "cloud_ops_manager_api_root_get_200" 
   }
 }
 
-resource "aws_api_gateway_integration" "cloud_ops_manager_api_root_get_ec2_integration" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_ops_manager_api.id
-  resource_id = aws_api_gateway_resource.cloud_ops_manager_api_root.id
-  http_method = aws_api_gateway_method.cloud_ops_manager_api_root_get.http_method
-
-  integration_http_method = "GET"
-  type                    = "HTTP"
-  uri                     = "http://${var.cloud_ops_manager_api_host}:5000/resource-provisioner"
-}
-
-resource "aws_api_gateway_integration_response" "cloud_ops_manager_api_ec2_integration_response_get_200" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_ops_manager_api.id
-  resource_id = aws_api_gateway_resource.cloud_ops_manager_api_root.id
-  http_method = aws_api_gateway_method.cloud_ops_manager_api_root_get.http_method
-  status_code = aws_api_gateway_method_response.cloud_ops_manager_api_root_get_200.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-
-  depends_on = [
-    aws_api_gateway_integration.cloud_ops_manager_api_root_get_ec2_integration
-  ]
-}
-
 resource "aws_api_gateway_method" "cloud_ops_manager_api_root_post" {
   rest_api_id      = aws_api_gateway_rest_api.cloud_ops_manager_api.id
   resource_id      = aws_api_gateway_resource.cloud_ops_manager_api_root.id
@@ -70,31 +45,6 @@ resource "aws_api_gateway_method_response" "cloud_ops_manager_api_root_post_202"
   response_models = {
     "application/json" = "Empty"
   }
-}
-
-resource "aws_api_gateway_integration" "cloud_ops_manager_api_root_post_ec2_integration" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_ops_manager_api.id
-  resource_id = aws_api_gateway_resource.cloud_ops_manager_api_root.id
-  http_method = aws_api_gateway_method.cloud_ops_manager_api_root_post.http_method
-
-  integration_http_method = "POST"
-  type                    = "HTTP"
-  uri                     = "http://${var.cloud_ops_manager_api_host}:5000/resource-provisioner"
-}
-
-resource "aws_api_gateway_integration_response" "cloud_ops_manager_api_ec2_integration_response_post_202" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_ops_manager_api.id
-  resource_id = aws_api_gateway_resource.cloud_ops_manager_api_root.id
-  http_method = aws_api_gateway_method.cloud_ops_manager_api_root_post.http_method
-  status_code = aws_api_gateway_method_response.cloud_ops_manager_api_root_post_202.status_code
-
-  response_templates = {
-    "application/json" = ""
-  }
-
-  depends_on = [
-    aws_api_gateway_integration.cloud_ops_manager_api_root_post_ec2_integration
-  ]
 }
 
 resource "aws_api_gateway_resource" "cloud_ops_manager_api_auth" {
@@ -120,59 +70,6 @@ resource "aws_api_gateway_integration" "cloud_ops_manager_api_auth_post_lambda_i
   uri                     = var.auth_lambda_invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "cloud_ops_manager_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.cloud_ops_manager_api.id
-
-  triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.cloud_ops_manager_api.body))
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  depends_on = [
-    aws_api_gateway_method.cloud_ops_manager_api_root_post,
-    aws_api_gateway_integration.cloud_ops_manager_api_root_post_ec2_integration,
-    aws_api_gateway_method_response.cloud_ops_manager_api_root_post_202,
-    aws_api_gateway_integration_response.cloud_ops_manager_api_ec2_integration_response_post_202,
-    aws_api_gateway_method.cloud_ops_manager_api_root_get,
-    aws_api_gateway_integration.cloud_ops_manager_api_root_get_ec2_integration,
-    aws_api_gateway_method_response.cloud_ops_manager_api_root_get_200,
-    aws_api_gateway_integration_response.cloud_ops_manager_api_ec2_integration_response_get_200
-  ]
-}
-
-resource "aws_api_gateway_stage" "cloud_ops_manager_api_dev_stage" {
-  stage_name    = "dev"
-  rest_api_id   = aws_api_gateway_rest_api.cloud_ops_manager_api.id
-  deployment_id = aws_api_gateway_deployment.cloud_ops_manager_api_deployment.id
-
-  tags = {
-    xray_tracing_enabled = "true"
-  }
-}
-
-resource "aws_api_gateway_usage_plan" "cloud_ops_manager_api_usage_plan" {
-  name        = "CloudOps Manager API Usage Plan"
-  description = "Usage plan for Cloud Ops Manager API"
-
-  api_stages {
-    api_id = aws_api_gateway_rest_api.cloud_ops_manager_api.id
-    stage  = aws_api_gateway_stage.cloud_ops_manager_api_dev_stage.stage_name
-  }
-
-  throttle_settings {
-    rate_limit  = 100
-    burst_limit = 50
-  }
-
-  depends_on = [
-    aws_api_gateway_stage.cloud_ops_manager_api_dev_stage,
-    aws_api_gateway_deployment.cloud_ops_manager_api_deployment
-  ]
-}
-
 resource "aws_api_gateway_api_key" "cloud_ops_manager_api_key" {
   name        = "CloudOps Manager API Key"
   description = "API Key for Cloud Ops Manager API"
@@ -181,11 +78,5 @@ resource "aws_api_gateway_api_key" "cloud_ops_manager_api_key" {
   tags = {
     Name = "CloudOps Manager API Key"
   }
-}
-
-resource "aws_api_gateway_usage_plan_key" "cloud_ops_manager_api_usage_plan_key" {
-  key_id        = aws_api_gateway_api_key.cloud_ops_manager_api_key.id
-  key_type      = "API_KEY"
-  usage_plan_id = aws_api_gateway_usage_plan.cloud_ops_manager_api_usage_plan.id
 }
 
