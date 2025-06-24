6e4ad9d592c4c1bf2cce396bd393a5a1711be7ef
Rafael Costa <rafaelcmd@gmail.com>
2025-06-24 18:58:18 -0300

    refactor: new project structure

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -47,3 +47,17 @@ module "ecs" {
   cloud_ops_manager_api_ecs_tg_arn        = module.alb.cloud_ops_manager_api_ecs_tg_arn
   cloud_ops_manager_api_ecs_listener      = module.alb.cloud_ops_manager_api_ecs_listener
 }
+
+module "shared_vpc" {
+  source               = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infra/modules/shared/aws/vpc?ref=main"
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones   = ["us-east-1a", "us-east-1b"]
+  project              = "cloudops"
+  environment          = "prod"
+  tags = {
+    Owner      = "platform-team"
+    CostCenter = "cloudops-prod"
+  }
+}
---
+++ infra/modules/shared/aws/vpc/variables.tf
@@ -0,0 +1,35 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of availability zones to deploy subnets into"
+  type        = list(string)
+}
+
+variable "project" {
+  description = "Name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (e.g. dev, staging, prod)"
+  type        = string
+}
+
+variable "tags" {
+  description = "Common resource tags"
+  type        = map(string)
+  default     = {}
+}
