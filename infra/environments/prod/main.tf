b998a4bfc6fcefaff22b9e013669dcd79382e7bf
Rafael Costa <rafaelcmd@gmail.com>
2025-07-12 20:45:15 -0300

    feat: added ALB module variables

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -1,11 +1,13 @@
 module "shared_vpc" {
-  source               = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infra/modules/shared/aws/vpc?ref=main"
+  source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infra/modules/shared/aws/vpc?ref=main"
+
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
   availability_zones   = ["us-east-1a", "us-east-1b"]
   project              = "cloudops"
   environment          = "prod"
+
   tags = {
     Owner      = "platform-team"
     CostCenter = "cloudops-prod"
@@ -18,4 +20,34 @@ module "ecs" {
 
 module "alb_resource_provisioner_api" {
   source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infra/modules/shared/aws/alb?ref=main"
+
+  alb_name           = "resource-provisioner-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.ecs.ecs_service_sg]
+  subnets            = module.shared_vpc.public_subnet_ids
+
+  target_group_name     = "resource-provisioner-tg"
+  target_group_port     = 5000
+  target_group_protocol = "HTTP"
+  vpc_id                = module.shared_vpc.vpc_id
+  target_type           = "ip"
+
+  health_check_path     = "/health"
+  health_check_interval = 30
+  health_check_timeout  = 5
+  healthy_threshold     = 3
+  unhealthy_threshold   = 2
+  matcher               = "200"
+
+  listener_port     = 80
+  listener_protocol = "HTTP"
+
+  project     = "cloudops"
+  environment = "prod"
+
+  tags = {
+    Environment = "prod"
+    Project     = "cloudops"
+  }
 }
