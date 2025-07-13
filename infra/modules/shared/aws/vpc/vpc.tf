01ec79fda50c291b41ac264c58ec6b947a302090
Rafael Costa <rafaelcmd@gmail.com>
2025-07-13 11:10:30 -0300

    feat: created NAT Gateway

--- infra/modules/shared/aws/vpc/vpc.tf
+++ infra/modules/shared/aws/vpc/vpc.tf
@@ -59,3 +59,24 @@ resource "aws_route_table_association" "public_subnets" {
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
+
+# Create NAT Gateway for private subnets
+resource "aws_eip" "nat" {
+  tags = {
+    Name = "cloud_ops_manager_nat_a"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat.allocation_id
+  subnet_id     = aws_subnet.public.id
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+}
