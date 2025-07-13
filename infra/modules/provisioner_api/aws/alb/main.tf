d75bc1870e1c05fe873a701bcb37ac934947b07c
Rafael Costa <rafaelcmd@gmail.com>
2025-07-13 13:21:26 -0300

    fix: adjusted ALB security group type

--- infra/modules/provisioner_api/aws/alb/main.tf
+++ infra/modules/provisioner_api/aws/alb/main.tf
@@ -2,7 +2,7 @@ resource "aws_lb" "this" {
   name               = var.alb_name
   internal           = var.internal
   load_balancer_type = "application"
-  security_groups    = aws_security_group.this.id
+  security_groups    = [aws_security_group.this.id]
   subnets            = var.subnets
 
   tags = merge(var.tags, {
