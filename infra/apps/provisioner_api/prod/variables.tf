12429ba731cbec25b922221af566fd56653ef976
Rafael Costa <rafaelcmd@gmail.com>
2025-08-13 20:33:35 -0300

    chore: set datadog test account api key

--- infra/apps/provisioner_api/prod/variables.tf
+++ infra/apps/provisioner_api/prod/variables.tf
@@ -2,6 +2,7 @@ variable "datadog_api_key" {
   description = "Datadog API key for monitoring"
   type        = string
   sensitive   = true
+  default     = "c791920ece74d7fb99afe0bb4ba85652"
 }
 
 variable "environment" {
