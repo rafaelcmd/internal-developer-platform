2d1a7f9cb93d25e6582c7aaf394abf57a2a27eb7
Rafael Costa <rafaelcmd@gmail.com>
2025-08-23 12:23:36 -0300

    fix: changed the datadog_forwarder_arn to export the invoke_arn

--- infra/modules/aws/lambda/outputs.tf
+++ infra/modules/aws/lambda/outputs.tf
@@ -1,6 +1,6 @@
-output "function_arn" {
+output "datadog_forwarder_arn" {
   description = "The ARN of the Lambda function"
-  value       = aws_lambda_function.this.arn
+  value       = aws_lambda_function.this.invoke_arn
 }
 
 output "function_name" {
