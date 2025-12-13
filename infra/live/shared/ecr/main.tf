0883d72225031879c18585ce0ecd90a1ae803bf2
Rafael Costa <rafaelcmd@gmail.com>
2025-12-13 17:41:12 -0300

    fix: gitbub repo name

--- infra/live/shared/ecr/main.tf
+++ infra/live/shared/ecr/main.tf
@@ -1,5 +1,5 @@
 module "ecr" {
-  source = "git::https://github.com/rafaelcmd/cloud-ops-manager.git//infra/modules/aws/ecr?ref=main"
+  source = "git::https://github.com/rafaelcmd/internal-developer-platform.git//infra/modules/aws/ecr?ref=main"
 
   repository_name = "cloudops-manager-repo"
 }
