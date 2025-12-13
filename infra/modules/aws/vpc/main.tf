54d02181392c4845caf8bad22d2e1dc41d3a1f38
Rafael Costa <rafaelcmd@gmail.com>
2025-12-13 17:22:29 -0300

    fix: created ecr vpc endpoints

--- infra/modules/aws/vpc/main.tf
+++ infra/modules/aws/vpc/main.tf
@@ -144,3 +144,65 @@ resource "aws_vpc_endpoint" "sqs" {
     Name = "${var.project}-${var.environment}-vpce-sqs"
   })
 }
+
+########################################
+# ECR API VPC Endpoint (Interface)
+########################################
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id              = aws_vpc.this.id
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.api"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.endpoints.id]
+  private_dns_enabled = true
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-vpce-ecr-api"
+  })
+}
+
+########################################
+# ECR DKR VPC Endpoint (Interface)
+########################################
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id              = aws_vpc.this.id
+  service_name        = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.endpoints.id]
+  private_dns_enabled = true
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-vpce-ecr-dkr"
+  })
+}
+
+########################################
+# CloudWatch Logs VPC Endpoint (Interface)
+########################################
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id              = aws_vpc.this.id
+  service_name        = "com.amazonaws.${var.aws_region}.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.endpoints.id]
+  private_dns_enabled = true
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-vpce-logs"
+  })
+}
+
+########################################
+# S3 VPC Endpoint (Gateway)
+########################################
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.${var.aws_region}.s3"
+  vpc_endpoint_type = "Gateway"
+  route_table_ids   = [aws_route_table.private.id]
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-vpce-s3"
+  })
+}
