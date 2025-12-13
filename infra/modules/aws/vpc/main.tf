c57a5b2421cd198688ae392cb5042154bbd0346d
Rafael Costa <rafaelcmd@gmail.com>
2025-12-13 19:22:13 -0300

    fix: added ssm vpc endpoint

--- infra/modules/aws/vpc/main.tf
+++ infra/modules/aws/vpc/main.tf
@@ -194,6 +194,22 @@ resource "aws_vpc_endpoint" "logs" {
 }
 
 ########################################
+# SSM VPC Endpoint (Interface)
+########################################
+resource "aws_vpc_endpoint" "ssm" {
+  vpc_id              = aws_vpc.this.id
+  service_name        = "com.amazonaws.${var.aws_region}.ssm"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.endpoints.id]
+  private_dns_enabled = true
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-vpce-ssm"
+  })
+}
+
+########################################
 # S3 VPC Endpoint (Gateway)
 ########################################
 resource "aws_vpc_endpoint" "s3" {
