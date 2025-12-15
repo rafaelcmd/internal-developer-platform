9247d5dd30cd091d9e32cddef0002e9071d9aeb7
Rafael Costa <rafaelcmd@gmail.com>
2025-12-15 12:47:52 -0300

    feat: created NAT Gateway

--- infra/modules/aws/vpc/main.tf
+++ infra/modules/aws/vpc/main.tf
@@ -101,6 +101,34 @@ resource "aws_route_table_association" "private_subnets" {
 }
 
 ########################################
+# NAT Gateway (for private outbound)
+########################################
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-nat-eip"
+  })
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-nat"
+  })
+
+  depends_on = [aws_internet_gateway.this]
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
+
+########################################
 # SG for interface endpoints
 ########################################
 resource "aws_security_group" "endpoints" {
