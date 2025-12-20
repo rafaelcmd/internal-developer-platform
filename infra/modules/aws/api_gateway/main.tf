76e68433d8e5d03d7a799537db20173f68522df5
Rafael Costa <rafaelcmd@gmail.com>
2025-12-20 19:57:03 -0300

    fix: adjusted api gateway to use nlb_listener_arn

--- infra/modules/aws/api_gateway/main.tf
+++ infra/modules/aws/api_gateway/main.tf
@@ -9,9 +9,8 @@ resource "aws_apigatewayv2_api" "this" {
   protocol_type = "HTTP"
 
   body = templatefile("${path.module}/../../../../api/docs/swagger.yaml", {
-    nlb_dns_name           = var.nlb_dns_name
+    nlb_listener_arn       = var.nlb_listener_arn
     vpc_link_id            = aws_apigatewayv2_vpc_link.this.id
-    integration_timeout_ms = var.integration_timeout_ms
   })
 
   fail_on_warnings = true
