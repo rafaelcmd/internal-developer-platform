3fc648dea9460b6444c8d76bd662228c67b2a254
Rafael Costa <rafaelcmd@gmail.com>
2025-12-21 15:03:06 -0300

    feat: created healthCheck to the api container

--- infra/modules/aws/ecs/task_definition.tf
+++ infra/modules/aws/ecs/task_definition.tf
@@ -21,6 +21,13 @@ resource "aws_ecs_task_definition" "api" {
       name      = var.app_container_name
       image     = var.app_image_uri
       essential = true
+      healthCheck = {
+        command     = ["CMD-SHELL", "wget --no-verbose --tries=1 --spider http://localhost:${var.container_port}/${var.environment}/health || exit 1"]
+        interval    = 30
+        retries     = 3
+        startPeriod = 60
+        timeout     = 5
+      }
       dependsOn = [
         {
           containerName = "datadog-agent"
