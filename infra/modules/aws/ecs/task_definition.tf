886d196036defb47a9651ff3aa5b3b54faf58206
Rafael Costa <rafaelcmd@gmail.com>
2025-12-21 08:52:20 -0300

    feat: changed the basePath to have the environment

--- infra/modules/aws/ecs/task_definition.tf
+++ infra/modules/aws/ecs/task_definition.tf
@@ -33,6 +33,7 @@ resource "aws_ecs_task_definition" "api" {
       }]
       environment = [
         { name = "PORT", value = tostring(var.container_port) },
+        { name = "ENVIRONMENT", value = var.environment },
         { name = "DD_SERVICE", value = var.service_name },
         { name = "DD_ENV", value = var.environment },
         { name = "DD_VERSION", value = var.app_version },
