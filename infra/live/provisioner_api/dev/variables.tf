876281c4baad31d0dee361866babf2e875bec8cc
Rafael Costa <rafaelcmd@gmail.com>
2025-12-25 14:05:28 -0300

    fix: Added a sane default for assume_role_policy

--- infra/live/provisioner_api/dev/variables.tf
+++ infra/live/provisioner_api/dev/variables.tf
@@ -369,6 +369,18 @@ variable "assume_role_policy" {
       })
     }))
   })
+  default = {
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  }
 }
 
 variable "lambda_basic_execution_policy_arn" {
