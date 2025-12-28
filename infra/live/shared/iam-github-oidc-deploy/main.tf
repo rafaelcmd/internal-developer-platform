e44a449ffcd16871b790d5a153ec269137ffef7a
Rafael Costa <rafaelcmd@gmail.com>
2025-12-28 15:56:25 -0300

    fix: OIDC modules and temporary workflows adjustments

--- infra/live/shared/iam-github-oidc-deploy/main.tf
+++ infra/live/shared/iam-github-oidc-deploy/main.tf
@@ -1,8 +1,12 @@
+data "tls_certificate" "github_oidc" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
 module "github_actions_oidc" {
   source         = "git::https://github.com/rafaelcmd/internal-developer-platform.git//infra/modules/aws/oidc?ref=main"
   url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
-  thumbprint     = var.github_thumbprint
+  thumbprint     = data.tls_certificate.github_oidc.certificates[0].sha1_fingerprint
   role_name      = var.github_role_name
   string_equals  = {
     "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
@@ -11,5 +15,5 @@ module "github_actions_oidc" {
     "token.actions.githubusercontent.com:sub" = var.github_allowed_subs
   }
   policy_arns    = var.github_policy_arns
-  tags           = var.tags
+  tags           = local.tags
 }
