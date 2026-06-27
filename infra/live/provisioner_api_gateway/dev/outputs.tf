8c2eeb55cd687687641860208ee22cdcba92143c
Rafael Costa <rafaelcmd@gmail.com>
2026-06-27 17:55:56 -0300

    fix: marked the outputs as sensitives

--- infra/live/provisioner_api_gateway/dev/outputs.tf
+++ infra/live/provisioner_api_gateway/dev/outputs.tf
@@ -29,9 +29,11 @@ output "vpc_link_id" {
 output "nlb_arn" {
   description = "ARN of the NLB the VPC Link targets"
   value       = data.aws_ssm_parameter.api_nlb_arn.value
+  sensitive   = true
 }
 
 output "nlb_dns_name" {
   description = "DNS name of the NLB the VPC Link targets"
   value       = data.aws_ssm_parameter.api_nlb_dns_name.value
+  sensitive   = true
 }
